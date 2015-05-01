5cc793e34b669282554740e492eb4db0f327eabc
Dag Martin Mikkelsen <reversi@gmail.com>
2015-05-01 11:59:15 +0200

    Update main.js

--- findagift/main.js
+++ findagift/main.js
@@ -1,35 +1,55 @@
 var genderDropdown;
+var ageDropdown;
+var priceDropdown;
 var genderOptions;
+var ageOptions;
+var priceOptions;
 var frontFilterOverlay;
 
 
 $(function(){
-    genderDropdown = $("#dd-gender");
-    genderOptions = $("#options-gender");
+    genderDropdown =     $("#dd-gender");
+    ageDropdown =        $("#dd-age");
+    priceDropdown =      $("#dd-price");
+    genderOptions =      $("#options-gender");
+    ageOptions =         $("#options-age");
+    priceOptions =       $("#options-price");
     frontFilterOverlay = $("#front-filter-overlay");
 
     genderDropdown.click(openGenderOptions);
+    ageDropdown   .click(openAgeOptions);
+    priceDropdown .click(openPriceOptions);
     frontFilterOverlay.click(closeAllOptions);
 });
 
 function openGenderOptions() {
-    var pos = genderDropdown.position();
+    openOptions(genderDropdown, genderOptions);
+}
+
+function openAgeOptions() {
+    openOptions(ageDropdown, ageOptions);
+}
+
+function openPriceOptions() {
+    openOptions(priceDropdown, priceOptions);
+}
+
+function openOptions(dropdown, options) {
+    var pos = dropdown.position();
     frontFilterOverlay.fadeIn(600);
-    genderOptions.css({
+    options.css({
         left: pos.left
     }).addClass("show");
 }
 
-function closeGenderOptions() {
-    genderOptions.removeClass("show");
-    setTimeout(function(){
-        genderOptions.css({
-            left: "-1000px"
-        })
-    }, 300);
-    frontFilterOverlay.fadeOut(600);
-}
-
 function closeAllOptions() {
-    closeGenderOptions();
+    genderOptions.removeClass("show");
+    ageOptions.removeClass("show");
+    priceOptions.removeClass("show");
+    frontFilterOverlay.fadeOut(300);
+    setTimeout(function(){
+        genderOptions.css("left", "-1000px");
+        ageOptions.css("left", "-1000px");
+        priceOptions.css("left", "-1000px");
+    }, 300);
 }
